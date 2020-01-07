ef679d3498d0121542f5653c20791ec56ac93483
timkeane <t.d.keane@gmail.com>
2020-01-07 14:14:08 -0500

    .

--- init/geoserver/init.sh
+++
@@ -1,7 +0,0 @@
-GS=http://admin:geoserver@localhost:8080/geoserver
-REST=$GS/rest/
-CT="Content-type: application/json"
-curl -X POST -H "$CT" -d @namespace.json $REST/namespaces.json
-curl -X POST -H "$CT" -d @datastore.json $REST/workspaces/basicgov/datastores.json
-curl -X POST -H "$CT" -d @featuretype.json $REST/workspaces/basicgov/datastores/postgis/featuretypes.json
-curl -X POST -H "Content-type: application/xml" -d @insert.xml $GS/wfs?REQUEST=TRANSACTION
---
+++ run.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+USER=admin
+PASS=geoserver
+HOST=localhost
+PORT=8080
+
+GS=$HOST:$PORT/geoserver
+WFS=http://$GS/wfs?REQUEST=GETCAPABILITIES
+WFST=http://$USER:$PASS@$GS/wfs?REQUEST=TRANSACTION
+REST=http://$USER:$PASS@$GS/rest
+DIR=./init/geoserver
+HDR="Content-type: application/json"
+
+docker-compose up -d
+
+while true
+do
+  sleep 5
+
+  STATUS=$(curl -s -o /dev/null -w '%{http_code}' $WFS)
+
+  if [ $STATUS -eq 200 ]; then
+    echo ''
+    echo "... Configuring GeoServer ..."
+    echo ''
+
+    curl -X POST -H "$HDR" -d @$DIR/namespace.json $REST/namespaces.json
+    echo ''
+    curl -X POST -H "$HDR" -d @$DIR/datastore.json $REST/workspaces/basicgov/datastores.json
+    echo ''
+    curl -X POST -H "$HDR" -d @$DIR/featuretype.json $REST/workspaces/basicgov/datastores/postgis/featuretypes.json
+    echo ''
+    curl -X POST -H "Content-type: application/xml" -d @$DIR/insert.xml $WFST
+    echo ''
+
+    echo ''
+    echo '============== GeoServer is configured! =============='
+    
+
+    break
+  else
+    echo ''
+    echo '... Waiting for GeoServer ...'
+  fi
+  sleep 5
+done
+
